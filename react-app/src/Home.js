cda429b1c4b85be7a6a9b3312b212181f315f37b
astroboy28 <eicalla2011@gmail.com>
2021-09-23 16:25:28 +0900

    error handling

--- react-app/src/Home.js
+++ react-app/src/Home.js
@@ -4,24 +4,33 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    const [name, setName] = useState('Mario');
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                console.log(res);
+                if(!res.ok){
+                    throw Error('Could not get the data');
+                }
                 return res.json();
             })
             .then(data => {
                 console.log(data);
                 setBlogs(data);
                 setIsLoading(false);
+            })
+            .catch(err => {
+                setError(err.message);
+                setIsLoading(false);
             });
     }, []);
 
     return ( 
         <div className="home">
-            {isLoading && <div>Loading...</div>}
+           {isLoading && <div>Loading...</div>}
+           {error && <div>{error}</div>}
            {blogs && <BlogList blogWatever={blogs} title="All Blogs"/>}
         </div>
      );
