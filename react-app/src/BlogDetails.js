9939e32e75f5e92703045d0776a04f690c1f1178
astroboy28 <eicalla2011@gmail.com>
2021-09-28 09:40:59 +0900

    Deleting Blogs

--- react-app/src/BlogDetails.js
+++ react-app/src/BlogDetails.js
@@ -1,11 +1,22 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
-
+import { useHistory } from "react-router-dom";
+// Programmatic Redirects
 
 
 const BlogDetails = () => {
     const { id } = useParams();   // Route parameter
     const { data: blogss, error, isLoading } = useFetch('http://localhost:8000/blogs/' + id); // Reusing Custom Hooks
+
+    // Delete an article/blog
+    const history = useHistory();
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blogss.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
 
     return ( 
         <div className="blog-details">
@@ -16,6 +27,7 @@ const BlogDetails = () => {
                    <h2>{ blogss.title }</h2>
                    <p>Written by: { blogss.author }</p>
                    <div>{ blogss.body }</div>
+                   <button onClick={handleClick}>Delete</button>
                </article>
            )}
         </div>
